42f883b46790af65e0260680d43723ae294179e0
jyothsna516 <60085648+jyothsna516@users.noreply.github.com>
2020-01-26 11:17:55 +0530

    Add files via upload

---
+++ Buyerbo.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmartApplication
+{
+    class BuyerBo
+    {
+        int id;
+        string name, phno, email;
+        ItemsBo ibo = new ItemsBo();
+        public BuyerBo(int id, string name, string phno, string email)
+        {
+            this.id = id;
+            this.name = name;
+            this.phno = phno;
+            this.email = email;
+            Buyer b = new Buyer(this.id, this.name, this.phno, this.email);
+        }
+        public void Search()
+        {
+            ibo.Search();
+        }
+        public void DisplayCategory()
+        {
+            ibo.DisplayProducts();
+
+        }
+        public void DisplayDetails()
+        {
+            Console.WriteLine("Details:");
+            Console.WriteLine("Id : " + id);
+            Console.WriteLine("Name : " + name);
+            Console.WriteLine("Phone Number : " + phno);
+            Console.WriteLine("Email : " + email);
+        }
+
+
+    }
+}
---
+++ Category.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmartApplication
+{
+    
+        abstract class Category
+        {
+            public int cid { get; set; }
+            public string cname { get; set; }
+            public string cdesc { get; set; }
+
+        }
+    }
+  
